0b8f6e4248a7b9cc80ecc4c8567608986719311e
moi <tdaniel.webdev@gmail.com>
2023-11-02 19:33:05 +0100

    presque email

--- app/models.py
+++ app/models.py
@@ -2,14 +2,36 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash 
 from flask_login import UserMixin
 
+
+
+cart_to_product = Table(
+    'user_to_role',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('role_id', Integer, ForeignKey('roles.id'))
+)
+
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name=db.Column(db.String(64), index=True, unique=True)
     product_price=db.Column(db.Integer)
     product_desc=db.Column(db.Text)
 
+
+
     def __repr__(self):
         return '<Product {}>'.format(self.product_name)
+
+def Cart(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(64), index=True, unique=True)
+
+    products = db.relationship('Product', secondary='', back_populates='carts')
+
+    def __repr__(self):
+        return '<Cart of client {}>'.format(self.username)
+
 
 
 class User(UserMixin, db.Model):
@@ -30,3 +52,4 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
--- config.py
+++ config.py
@@ -6,7 +6,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
     STATIC_FOLDER='/app/static'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp@gmail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'tommyukeman@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'kezi fjfp vcls lwns'
+    ADMINS = ['tommyukeman@gmail.com']
